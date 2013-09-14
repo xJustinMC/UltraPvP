ecb7b2ab88aead725e0532ad2cf849e3a3cedbe4
xJustinMC <xJustinMC@gmail.com>
2013-09-13 22:24:38 -0400

    UltraPvP

--- UltraPvP/src/me/xJustinMC/plugin/UltraPvP/UltraPvP.java
+++ UltraPvP/src/me/xJustinMC/plugin/UltraPvP/UltraPvP.java
@@ -1,9 +1,11 @@
 package me.xJustinMC.plugin.UltraPvP;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UltraPvP extends JavaPlugin{
-	
+	public Logger logger = Logger.getLogger("Minecraft");
 	
 	public void onEnable(){
 		
