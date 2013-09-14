9ae86d6f670dcf2e47e180f6f228897a0b274958
xJustinMC <xJustinMC@gmail.com>
2013-09-13 23:05:01 -0400

    UltraPvP

--- UltraPvP/src/me/xJustinMC/plugin/UltraPvP/UltraPvP.java
+++ UltraPvP/src/me/xJustinMC/plugin/UltraPvP/UltraPvP.java
@@ -2,6 +2,8 @@ package me.xJustinMC.plugin.UltraPvP;
 
 import java.util.logging.Logger;
 
+import me.xJustinMC.plugin.UltraPvP.commands.Heal;
+
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,8 +13,13 @@ public class UltraPvP extends JavaPlugin{
 	public void onEnable(){
 		PluginDescriptionFile pdfFile = getDescription();
 		this.logger.info("UltraPvP by xJustinMC v" + pdfFile.getVersion() + " has been enabled!");
+		Commands();
 	}
 	
+	private void Commands() {
+		getCommand("heal").setExecutor(new Heal(this));	
+	}
+
 	public void onDisable(){
 		PluginDescriptionFile pdfFile = getDescription();
 		this.logger.severe("UltraPvP by xJustinMC v" + pdfFile.getVersion() + " has been disabled!");
