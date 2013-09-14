4bbba9285430269001a58f0ab8cdb478b1ed5414
xJustinMC <xJustinMC@gmail.com>
2013-09-13 22:33:37 -0400

    UltraPvP

--- UltraPvP/src/me/xJustinMC/plugin/UltraPvP/UltraPvP.java
+++ UltraPvP/src/me/xJustinMC/plugin/UltraPvP/UltraPvP.java
@@ -2,16 +2,20 @@ package me.xJustinMC.plugin.UltraPvP;
 
 import java.util.logging.Logger;
 
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class UltraPvP extends JavaPlugin{
 	public Logger logger = Logger.getLogger("Minecraft");
 	
 	public void onEnable(){
-		
+		PluginDescriptionFile pdfFile = getDescription();
+		this.logger.info("UltraPvP by xJustinMC v" + pdfFile.getVersion() + " has been enabled!");
 	}
 	
 	public void onDisable(){
+		PluginDescriptionFile pdfFile = getDescription();
+		this.logger.severe("UltraPvP by xJustinMC v" + pdfFile.getVersion() + " has been disabled!");
 		
 	}
 }
---
+++ UltraPvP/src/me/xJustinMC/plugin/UltraPvP/commands/Heal.java
@@ -0,0 +1,17 @@
+package me.xJustinMC.plugin.UltraPvP.commands;
+
+import me.xJustinMC.plugin.UltraPvP.UltraPvP;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class Heal {
+	public UltraPvP plugin;
+	public Heal(UltraPvP instance){
+		instance = plugin;
+	}
+	
+	public void onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		
+	}
+}
