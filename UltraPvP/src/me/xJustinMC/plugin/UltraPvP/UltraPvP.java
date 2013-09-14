a5d8e85c1e9ddb3cef25dfa3f59e0828b153f94c
xJustinMC <xJustinMC@gmail.com>
2013-09-13 22:21:05 -0400

    UltraPvP

---
+++ UltraPvP/src/me/xJustinMC/plugin/UltraPvP/UltraPvP.java
@@ -0,0 +1,15 @@
+package me.xJustinMC.plugin.UltraPvP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UltraPvP extends JavaPlugin{
+	
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+}
